b87ac208f5f58297055bd0ad3e8a9eef1bc17c01
Cowington Post <cowingtonpost@gmail.com>
2023-01-04 19:46:32 -0800

    add motor power actions

--- src/main/java/frc/robot/ShedOIDevice.java
+++ src/main/java/frc/robot/ShedOIDevice.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import org.xero1425.base.actions.Action;
+import org.xero1425.base.subsystems.motorsubsystem.MotorPowerAction;
+import org.xero1425.base.subsystems.motorsubsystem.MotorSubsystem;
 import org.xero1425.base.subsystems.oi.OIPanel;
 import org.xero1425.base.subsystems.oi.OISubsystem;
 import org.xero1425.base.subsystems.oi.OILed.State;
@@ -11,6 +14,8 @@ import org.xero1425.misc.MissingParameterException;
 import org.xero1425.base.actions.InvalidActionRequest;
 
 public class ShedOIDevice extends OIPanel {
+    private MotorPowerAction motor1PowerAction;
+    private MotorPowerAction motor2PowerAction;
 
     public ShedOIDevice(OISubsystem parent, String name, int index) throws BadParameterTypeException, MissingParameterException {
         super(parent, name, index) ;
@@ -22,6 +27,13 @@ public class ShedOIDevice extends OIPanel {
 
     @Override
     public void createStaticActions() throws Exception {
+        var robotSubsystem = (ShedOIRobotSubsystem) getSubsystem().getRobot().getRobotSubsystem();
+        var motor1Subsystem = robotSubsystem.getMotor1Subsystem();
+        var motor2Subsystem = robotSubsystem.getMotor2Subsystem();
+
+        motor1PowerAction = new MotorPowerAction(motor1Subsystem, 0);
+        motor2PowerAction = new MotorPowerAction(motor2Subsystem, 0);
+
     }
 
     @Override
@@ -31,9 +43,6 @@ public class ShedOIDevice extends OIPanel {
 
     private void initializeGadgets() throws BadParameterTypeException, MissingParameterException {
         int num ;
-
-       // num = getSubsystem().getSettingsValue("panel:gadgets:shoot_collect_mode").getInteger();
-       // collect_v_shoot_gadget_ = mapButton(num, OIPanelButton.ButtonType.Level);
 
     }
 }
--- src/main/java/frc/robot/ShedOIRobotSubsystem.java
+++ src/main/java/frc/robot/ShedOIRobotSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import org.xero1425.base.XeroRobot;
 import org.xero1425.base.subsystems.RobotSubsystem;
+import org.xero1425.base.subsystems.motorsubsystem.MotorSubsystem;
 import org.xero1425.base.subsystems.swerve.common.SwerveBaseSubsystem;
 import org.xero1425.base.subsystems.swerve.sdsswerve.SDSSwerveDriveSubsystem;
 import org.xero1425.base.subsystems.vision.LimeLightSubsystem;
@@ -9,8 +10,20 @@ import org.xero1425.base.subsystems.vision.LimeLightSubsystem;
 
 public class ShedOIRobotSubsystem extends RobotSubsystem {
 
+    private MotorSubsystem motor1Subsystem;
+    private MotorSubsystem motor2Subsystem;
     public ShedOIRobotSubsystem(XeroRobot robot) throws Exception {
         super(robot, "ShedOIRobotSubsystem") ;
 
+        motor1Subsystem = new MotorSubsystem(this, "motor1Subsystem");
+        motor2Subsystem = new MotorSubsystem(this, "motor2Subsystem");
+
+    }
+
+    public MotorSubsystem getMotor1Subsystem() {
+        return motor1Subsystem;
+    }
+    public MotorSubsystem getMotor2Subsystem() {
+        return motor2Subsystem;
     }
 }
