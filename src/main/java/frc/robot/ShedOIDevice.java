8efaab6bb7ad31e7f6d2722f9dd41c2fdbdbea3a
aditi <aditi.bhaskar@gmail.com>
2023-01-04 20:39:05 -0800

    added enable, reverse, and adjust oi buttons

--- src/main/java/frc/robot/ShedOIDevice.java
+++ src/main/java/frc/robot/ShedOIDevice.java
@@ -17,8 +17,12 @@ import org.xero1425.base.actions.InvalidActionRequest;
 public class ShedOIDevice extends OIPanel {
     private MotorPowerAction motor1PowerAction;
     private MotorPowerAction motor2PowerAction;
-    private int motor1_;
-    private int motor2_;
+    private int motor1_enable_;
+    private int motor2_enable_;
+    private int motor1_reverse_;
+    private int motor2_reverse_;
+    private int motor1_adjust_; // read this analog value -1 to 1 directly
+    private int motor2_adjust_; // read this analog value -1 to 1 directly
 
     public ShedOIDevice(OISubsystem parent, String name, int index) throws BadParameterTypeException, MissingParameterException {
         super(parent, name, index) ;
@@ -45,34 +49,63 @@ public class ShedOIDevice extends OIPanel {
 
     @Override
     public void generateActions() throws InvalidActionRequest {
+        int sign1 = 1 ;
+        int sign2 = 1 ;
+
+        //TODO: update motor1PowerAction with analog power (-1 to 1) scaled to (0 to 1) power
 
         //motor 1 actions
-        if (getValue(motor1_) == 0) {
-            motor1Subsystem.setAction(motor1POffAction) ;         
+        if (getValue(motor1_enable_) == 0) {
+            motor1Subsystem.setAction(motor1OffAction) ;         
         }
-        if (getValue(motor1_) == 1) {
-            motor1Subsystem.setAction(motor1PowerAction) ;       
+        if (getValue(motor1_enable_) == 1) {
+            if (getValue(motor1_reverse_) == 1) {//reverse
+                sign1 = -1 ;
+            }
+            motor1Subsystem.setAction(motor1PowerAction*sign1) ;       
         }
 
         //motor 2 actions
-        if (getValue(motor2_) == 0) {
-            motor1Subsystem.setAction(motor1POffAction) ; 
+        if (getValue(motor2__enable) == 0) {
+            motor1Subsystem.setAction(motor1OffAction) ; 
         }
-        if {
-            motor1Subsystem.setAction(motor1PowerAction) ;       
+        if (getValue(motor2_enable_) == 1) {
+            if (getValue(motor2_reverse_) == 1) {//reverse
+                sign2 = -1 ;
+            }
+            motor1Subsystem.setAction(motor1PowerAction*sign2) ;       
         }
 
+    }
+
+    @Override
+    public void computeState() throws Exception {
+        super.computeState() ;
+
+        double v = DriverStation.getStickAxis(getIndex(), item.getItemNumber()) ;
     }
 
     private void initializeGadgets() throws BadParameterTypeException, MissingParameterException {
         int num ;
         
         //TODO: add motor1 and motor2 to jsonc file
-        num = getSubsystem().getSettingsValue("panel:gadgets:motor1").getInteger();
-        motor1_ = mapButton(num, OIPanelButton.ButtonType.Level);
+        num = getSubsystem().getSettingsValue("panel:gadgets:motor1_enable").getInteger();
+        motor1_enable_ = mapButton(num, OIPanelButton.ButtonType.Level);
         
-        num = getSubsystem().getSettingsValue("panel:gadgets:motor2").getInteger();
-        motor2_ = mapButton(num, OIPanelButton.ButtonType.Level);
+        num = getSubsystem().getSettingsValue("panel:gadgets:motor2_enable").getInteger();
+        motor2_enable_ = mapButton(num, OIPanelButton.ButtonType.Level);
+
+        num = getSubsystem().getSettingsValue("panel:gadgets:motor1_reverse").getInteger();
+        motor1_reverse_ = mapButton(num, OIPanelButton.ButtonType.Level);
+        
+        num = getSubsystem().getSettingsValue("panel:gadgets:motor2_reverse").getInteger();
+        motor2_reverse_ = mapButton(num, OIPanelButton.ButtonType.Level);
+                
+        num = getSubsystem().getSettingsValue("panel:gadgets:motor1_adjust").getInteger();
+        motor1_adjust_ = mapButton(num, OIPanelButton.ButtonType.Level);
+
+        num = getSubsystem().getSettingsValue("panel:gadgets:motor2_adjust").getInteger();
+        motor2_adjust_ = mapButton(num, OIPanelButton.ButtonType.Level);
 
     }  
 }
